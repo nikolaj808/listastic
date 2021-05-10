534e9b68476365715623e0420e41e3fd88b28b6e
Nikolaj Kristensen <nikoak2@live.dk>
2021-05-10 22:16:52 +0200

    Removed status bar manipulation in main_production

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,27 +1,15 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'dart:async';
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:bloc/bloc.dart';
 import 'package:listastic/app/app.dart';
 import 'package:listastic/app/app_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-  );
 
   await Firebase.initializeApp();
 
